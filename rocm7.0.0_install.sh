6134d69294dc410fa09425a8d1b80f48a8e3dbac
KTK <alexander.malli@icloud.com>
2025-09-20 09:49:07 +0200

    Update rocm7.0.0_install.sh

--- rocm7.0.0_install.sh
+++ rocm7.0.0_install.sh
@@ -38,7 +38,7 @@ sudo apt install -y "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)
 sudo apt install -y amdgpu-dkms rocm rocm-opencl-runtime
 
 echo "=== Schritt 4: ROCm Pfade konfigurieren ==="
-sudo tee --append /etc/ld.so.conf.d/rocm.conf <<EOF
+sudo tee --append -y /etc/ld.so.conf.d/rocm.conf <<EOF
 /opt/rocm/lib
 /opt/rocm/lib64
 EOF
